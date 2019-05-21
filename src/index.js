f0c78d77eb4124f284a3bade20eb73fe52ad6159
Cristian Vergara <cvergarafuentes@correo.unicordoba.edu.co>
2019-05-21 22:20:37 +0000

    modificacion

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Sotracor");
+  res.send("Bienvenidos a sotracor");
 });
 
 app.get("/registros", (req, res) => {
